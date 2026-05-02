9b0db1d7abfe08715afd62dc3adde6122379f7d4
srkdev404-cyber <srkdev404@gmail.com>
2026-05-02 16:07:36 +0530

    Add functionality to send query and data to server

--- cli/copilot.py
+++ cli/copilot.py
@@ -1,1 +1,14 @@
+import requests
+import sys
 
+query = sys.argv[1]
+
+with open("data/logs.json") as f:
+    data = f.read()
+
+res = requests.post("http://localhost:8000/copilot", params={
+    "query": query,
+    "data": data
+})
+
+print(res.json()["response"])
