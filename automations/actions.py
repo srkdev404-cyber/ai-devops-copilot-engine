bee60006a3c9e1299a64990b7167797afb118901
srkdev404-cyber <srkdev404@gmail.com>
2026-05-02 16:08:20 +0530

    Add functions to restart ECS service and rerun pipeline

--- automations/actions.py
+++ automations/actions.py
@@ -1,1 +1,7 @@
+import subprocess
 
+def restart_ecs(service):
+    subprocess.run(["aws", "ecs", "update-service", "--force-new-deployment"])
+
+def rerun_pipeline():
+    print("Triggering pipeline...")
